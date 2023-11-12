586eb2272fb102dfb13ce27f4ef68c348e0724c9
TomasTatra <t.vikiszaly@gmail.com>
2023-11-12 20:56:29 +0100

    Pridanie stránky pre autá v grupáží (Carcontroler a /car/car_view) úprava a pridanie správ v messages a presmerovanie zo zadávania EČV na stránku s autami (zatiaľ nefunkčné..)

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -13,23 +13,23 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route(name:'app_car_group_')]
-class CarGroupController extends AbstractController
+#[Route(name:'app_car_')]
+class CarController extends AbstractController
 {
     #[Route('/{_locale<%app.supported_locales%>}/group-{id}', name: 'view')]
     public function view(CarGroup $carGroup, Request $request, ManagerRegistry $managerRegistry): Response
     {
         $form = $this->createForm(CarGroupType::class, $carGroup);
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $manager = $managerRegistry->getManager();
-            $manager->flush();
-//            return $this->redirectToRoute('app_car_view', [
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $manager = $managerRegistry->getManager();
+//            $manager->flush();
+//            return $this->redirectToRoute('app_cars_view', [
 //                'id' => $carGroup->getId(),
 //            ]);
-        }
+//        }
 
-        return $this->render('app/car_group/view.html.twig', [
+        return $this->render('app/car/car_view.html.twig', [
             'form' => $form,
             'carGroup' => $carGroup,
         ]);
