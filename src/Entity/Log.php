60fad439570a9a456dda52ea27d163f5ee8a72e7
RobertPagac <125203444+RobertPagac@users.noreply.github.com>
2024-01-01 15:01:28 +0100

    Pridanie chýbajúcich informácií v logu + logovanie zvyšných vecí

--- src/Entity/Log.php
+++ src/Entity/Log.php
@@ -20,6 +20,16 @@ class Log
     #[ORM\Column(length: 255)]
     private ?string $log = null;
 
+    #[ORM\Column]
+    private ?int $admin_id = null;
+
+    #[ORM\Column]
+    private ?int $object_id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\Length(max: 25)]
+    private ?string $object_class = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -47,5 +57,35 @@ class Log
         $this->log = $log;
 
         return $this;
+    }
+
+    public function getAdminId(): ?int
+    {
+        return $this->admin_id;
+    }
+
+    public function setAdminId(?int $admin_id): void
+    {
+        $this->admin_id = $admin_id;
+    }
+
+    public function getObjectId(): ?int
+    {
+        return $this->object_id;
+    }
+
+    public function setObjectId(?int $object_id): void
+    {
+        $this->object_id = $object_id;
+    }
+
+    public function getObjectClass(): ?string
+    {
+        return $this->object_class;
+    }
+
+    public function setObjectClass(?string $object_class): void
+    {
+        $this->object_class = $object_class;
     }
 }
